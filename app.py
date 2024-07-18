43503d4fb2a10302d0e3fd2c476be0b2cd74d23e
Juliana <juliana.li@csedge.org>
2024-07-18 10:42:16 -0700

    added flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, url_for
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def fill_in_blank():
+    
+@app.route('/complete')
+def compl():
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
